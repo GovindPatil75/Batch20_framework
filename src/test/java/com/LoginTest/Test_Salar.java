a700a8196df002a09911e670a017fbf0d8d331d4
unknown <salarsayyad123@gmail.com>
2023-08-27 00:13:03 +0530

    Test Salar

---
+++ src/test/java/com/LoginTest/Test_Salar.java
@@ -0,0 +1,11 @@
+package com.LoginTest;
+
+import org.testng.annotations.Test;
+
+public class Test_Salar {
+
+	@Test
+	public void framworkFlow() {
+		System.out.println("Understanding the flow of execution of framework");
+	}
+}
